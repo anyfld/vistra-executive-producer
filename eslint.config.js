dbeb0910a85bf46e2ec9409d1243f2e5c186d0b6
Itsuki Shirahige <shiron4710.dev@gmail.com>
2025-12-19 13:18:56 +0900

    chore(gen): update global ignores in ESLint configuration (#20)

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist', 'gen']),
+  globalIgnores(['dist', 'src/gen']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
