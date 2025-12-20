bc2165b9a9ae67916bbc0ed36b1a2ee398af232c
shiron <shiron4710.dev@gmail.com>
2025-12-20 11:22:49 +0900

    feat(webrtc): integrate global configuration for WebRTC stream source

--- src/components/WebRTCPlayer.tsx
+++ src/components/WebRTCPlayer.tsx
@@ -1,15 +1,28 @@
 import { useMemo } from "react"
+import { useQuery } from "@connectrpc/connect-query"
 import { Box } from "@mui/material"
+import { getGlobalConfig } from "@/gen/proto/v1/service-ConfigService_connectquery"
+import { create } from "@bufbuild/protobuf"
+import { GetGlobalConfigRequestSchema } from "@/gen/proto/v1/service_pb"
 
 interface WebRTCPlayerProps {
   name: string
 }
 
 export default function WebRTCPlayer({ name }: WebRTCPlayerProps) {
-  const streamSrc = useMemo(
-    () => `${import.meta.env.VITE_WEBRTC_BASE_URL}/webrtc.html?src=${name}&media=video+audio`,
-    [name]
-  )
+  const { data: globalConfig } = useQuery(getGlobalConfig, create(GetGlobalConfigRequestSchema, {}))
+
+  const streamSrc = useMemo(() => {
+    const template = globalConfig?.config?.webrtcUrlTemplate
+    if (!template) {
+      return ""
+    }
+    return template.replace("{webrtc_connection_name}", name)
+  }, [globalConfig?.config?.webrtcUrlTemplate, name])
+
+  if (!streamSrc) {
+    return null
+  }
 
   return (
     <Box
